db30487e4fbc56d0151e6a5d6bab7aa9be18f802
Amaan Ahmad <amaanahmad582001@gmail.com>
2022-03-09 13:21:46 +0530

    fixes for v1.2 (refer changelog)

--- src/recorder.js
+++ src/recorder.js
@@ -1,8 +1,8 @@
-let completeBlob = null;
+let unSeekableBlob = null;
 let stream = null;
 let chunks = [];
 let recorder = null;
-let options = { mimeType: "video/webm;codecs=vp9" };
+let options = { mimeType: "video/webm;codecs=vp9,opus" };
 
 document.getElementById("start-btn").addEventListener("click", startRec);
 document.getElementById("stop-btn").addEventListener("click", stopRec);
@@ -22,12 +22,24 @@ async function startRec() {
 
     recorder = new MediaRecorder(stream, options);
     recorder.ondataavailable = (e) => chunks.push(e.data);
-    recorder.start();
     recorder.onstop = onStop;
+    stream.getTracks().forEach((track) => {
+      track.onended = onTracksEnd;
+    });
+    recorder.start();
     document.getElementById("start-btn").style.display = "none";
     document.getElementById("stop-btn").style.display = "unset";
   } catch (error) {
     console.error(error);
+  }
+}
+
+function onTracksEnd() {
+  const allTracksEnded = stream
+    .getTracks()
+    .every((track) => track.readyState === "ended");
+  if (allTracksEnded) {
+    recorder.stop();
   }
 }
 
@@ -41,7 +53,9 @@ const download = function (data) {
 async function stopRec() {
   try {
     recorder.stop();
-    stream.getTracks().forEach((track) => {
+    const tracks = stream.getTracks();
+    console.log(tracks);
+    tracks.forEach((track) => {
       track.stop();
     });
   } catch (error) {
@@ -52,21 +66,54 @@ async function stopRec() {
 async function onStop() {
   try {
     document.getElementById("stop-btn").style.display = "none";
-
-    completeBlob = new Blob(chunks, {
+    unSeekableBlob = new Blob(chunks, {
       type: "video/webm",
     });
-    let downloadBtn = document.getElementById("download-btn");
-    let video = document.getElementById("vidPlayer");
-    let ObjectURL = URL.createObjectURL(completeBlob);
-    video.style.display = "block";
-    video.src = ObjectURL;
-    downloadBtn.style.display = "unset";
-    downloadBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      download(ObjectURL);
-    });
+    getSeekableBlob(unSeekableBlob, prepareDownload);
   } catch (error) {
     console.error(error);
   }
+}
+
+function prepareDownload(newBlob) {
+  let downloadBtn = document.getElementById("download-btn");
+  let video = document.getElementById("vidPlayer");
+  let ObjectURL = URL.createObjectURL(newBlob);
+  video.style.display = "block";
+  video.src = ObjectURL;
+  downloadBtn.style.display = "unset";
+  downloadBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    download(ObjectURL);
+  });
+}
+
+// Thanks for the answer https://stackoverflow.com/a/67823820/8986598
+function getSeekableBlob(inputBlob, callback) {
+  // EBML.js copyrights goes to: https://github.com/legokichi/ts-ebml
+  if (typeof EBML === "undefined") {
+    throw new Error("Please link: https://cdn.webrtc-experiment.com/EBML.js");
+  }
+  var reader = new EBML.Reader();
+  var decoder = new EBML.Decoder();
+  var tools = EBML.tools;
+  var fileReader = new FileReader();
+  fileReader.onload = function (e) {
+    var ebmlElms = decoder.decode(this.result);
+    ebmlElms.forEach(function (element) {
+      reader.read(element);
+    });
+    reader.stop();
+    var refinedMetadataBuf = tools.makeMetadataSeekable(
+      reader.metadatas,
+      reader.duration,
+      reader.cues
+    );
+    var body = this.result.slice(reader.metadataSize);
+    var newBlob = new Blob([refinedMetadataBuf, body], {
+      type: "video/webm",
+    });
+    callback(newBlob);
+  };
+  fileReader.readAsArrayBuffer(inputBlob);
 }
